9d3866bd159f9a3566d9c77508fd6fe4df2514cf
quakeroatsgod <agnellogriffen@gmail.com>
2023-05-13 07:24:33 -0700

    model works and is hooked up with bot

--- app.py
+++ app.py
@@ -1,37 +1,45 @@
 import os
+import argparse
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 # Local modules
 import handler
+if __name__ == "__main__":
+    # Get command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', action='store_true')
+    args = parser.parse_args()
 
-# Get env variables
-load_dotenv()
-TOKEN = os.getenv( 'DISCORD_TOKEN' )
-LOG_CHANNEL_ID = os.getenv( 'LOG_CHANNEL_ID' )
-OWNER_ID = os.getenv( 'OWNER_ID' )
+    # Get env variables
+    load_dotenv()
+    TOKEN = os.getenv( 'DISCORD_TOKEN' )
+    LOG_CHANNEL_ID = os.getenv( 'LOG_CHANNEL_ID' )
+    OWNER_ID = os.getenv( 'OWNER_ID' )
 
-intents = discord.Intents.default()
-intents.message_content = True
-# bot_client = discord.Client( intents=intents )
-bot = commands.Bot( command_prefix='$', intents=intents )
+    intents = discord.Intents.default()
+    intents.message_content = True
+    # bot_client = discord.Client( intents=intents )
+    bot = commands.Bot( command_prefix='$', intents=intents )
 
-# Event that prints sucessful login
-@bot.event
-async def on_ready():
-    """ Adds commands to bot """
-    await bot.add_cog( handler.Handler( bot ) )
-    print( f'Logged in as {bot.user}' )
-    await bot.get_channel( int(LOG_CHANNEL_ID) ).send(content="Quaker bot is online")
-    return
+    # Event that prints sucessful login
+    @bot.event
+    async def on_ready():
+        """ Adds commands to bot """
+        await bot.add_cog( handler.Handler( bot ) )
+        if ( args.verbose ):
+            print( f'Logged in as {bot.user}' )
+            await bot.get_channel( int(LOG_CHANNEL_ID) ).send(content="Quaker bot is online")
+        return
 
-@bot.command(name="sync")
-@commands.has_permissions(administrator=True)
-async def sync_commands_with_bot( ctx ):
-    """ Synchronizes commands with the bot """
-    await bot.add_cog( handler.Handler( bot ) )
-    await ctx.channel.send( "Done!" )
-    return
+    @bot.command(name="sync")
+    @commands.has_permissions(administrator=True)
+    async def sync_commands_with_bot( ctx ):
+        """ Synchronizes commands with the bot """
+        await bot.add_cog( handler.Handler( bot ) )
+        await ctx.channel.send( "Done!" )
+        return
 
-bot.run(TOKEN)
+    bot.run(TOKEN)
--- handler.py
+++ handler.py
@@ -1,11 +1,11 @@
 import discord
 import discord.ext.commands as commands
+import model
 
 class Handler( commands.Cog ):
     def __init__( self, bot ):
         self.bot = bot
-        self.model_loaded = False
-
+        self.model = None
     @commands.Cog.listener()
     async def on_member_join(self, member):
         pass
@@ -13,17 +13,35 @@ class Handler( commands.Cog ):
     @commands.command()
     async def hello( self, ctx ):
         """Says hello"""
-        await ctx.message( "Hello" )
+        return await ctx.channel.send( f"Hello {ctx.author.name}" )
+
+    @commands.command()
+    async def train( self, ctx ):
+        """ Trains the ML model with the training data """
+        # If the model was already trained
+        if ( self.model):
+            await ctx.channel.send( "Model is already trained and ready to use." )
+        else:
+            await ctx.channel.send( "Training the model..." )
+            self.model = model.train_model( False )
+            await ctx.channel.send( "Model is trained!" )
         return
 
     @commands.command()
-    async def load_model( self, ctx ):
-        """ Trains the ML model with the training data"""
-        # If the model was already trained
-        if ( self.model_loaded ):
-            await ctx.message( "Model is already trained and ready to use." )
-        else:
-            await ctx.message( "Training the model..." )
-            self.model_loaded = True
-        return
+    async def predict( self, ctx, *args ):
+        """ Predicts with trained ML model """
+        # If the model was not trained yet, don't allow predictions
+        if ( not self.model ):
+            return await ctx.channel.send( "Cannot predict. The model is not trained yet" )
+        # Assemble message to send to model
+        message = ' '.join( args )
+        # Get response message from the prediction
+        response = model.predict_message( self.model, message )
+        return await ctx.channel.send( response )
+
+    @commands.command()
+    async def reset( self, ctx ):
+        """ Resets the model to 'None' """
+        self.model = None
+        return await ctx.channel.send( "Cleared the model" )
 
